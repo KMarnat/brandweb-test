65399aa97cce33b1516ce8a83323bee59c238f83
Kaspar Marnat <kasparmarnat@gmail.com>
2023-08-28 16:28:18 +0300

    fixed skeleton card while loading

--- src/components/SkeletonCard.js
+++ src/components/SkeletonCard.js
@@ -6,13 +6,17 @@ import repost from '../assets/repost.svg';
 export function SkeletonCard() {
   return (
     <article className="card">
-      <div className="card__poster skeleton"></div>
+      <div className="adaptive">
+        <div className="adaptive-photo">
+          <div alt="game poster" className="card__poster skeleton"></div>
+        </div>
+      </div>
       <div className="card__content">
-        <div className="description">
+        <div className="card__description skeleton">
           <h2 className="skeleton">placeholder</h2>
           <h2 className="skeleton">placeholder</h2>
         </div>
-        <div className="icons">
+        <div className="card__icons">
           <img src={participate} alt="crosshair icon" />
           <img src={addfriend} alt="add friend icon" />
           <img src={addfavorite} alt="add favorite icon" />
