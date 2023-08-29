9514e0f4744d8dfe4401f1b738e22253865289d9
Kaspar Marnat <kasparmarnat@gmail.com>
2023-08-29 15:06:04 +0300

    give filterbutton component its own scss file, changed its class name and gave a modifier, instead of .active class

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,7 +1,7 @@
 export function FilterButton({ KEY, handleFiltering, activeTab, filter }) {
   return (
     <button
-      className={activeTab === filter.activeTab ? 'active' : ''}
+      className={activeTab === filter.activeTab ? 'filter__btn filter__btn--active' : 'filter__btn'}
       onClick={() =>
         handleFiltering(
           `https://api.rawg.io/api/games?key=${KEY}&${filter.apiRoute}`,
