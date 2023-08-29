4286d5421be2fbf3621c776e378a5eb86af737b0
Kaspar Marnat <kasparmarnat@gmail.com>
2023-08-29 11:50:30 +0300

    fixed loading effect for selected game

--- src/components/SkeletonSelect.js
+++ src/components/SkeletonSelect.js
@@ -3,10 +3,11 @@ import { GameDetail } from './GameDetail';
 export function SkeletonSelect() {
   return (
     <>
-      <div className="selected--left skeleton">
+      <div className="selected__overview skeleton--selected">
+        <img className="selected__overview-cover" alt="game poster" />
         <GameDetail title="" detail="" />
       </div>
-      <div className="selected--right skeleton">
+      <div className="selected__detail-grid skeleton--selected">
         <>
           <GameDetail title="" detail={''} />
           <GameDetail title="" detail={''} />
