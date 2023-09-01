20cd91cd99f0c0c7764313665fd522d12efed9ec
Kaspar Marnat <kasparmarnat@gmail.com>
2023-09-01 14:51:06 +0300

    responsivness of the profile page

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,14 +1,46 @@
 import profilemenu from '../assets/threedots.svg';
+import { useRef, useEffect, useState } from 'react';
 
 export default function ProfileCard() {
+  const [avatarHeight, setAvatarHeight] = useState(0);
+  const avatarRef = useRef(null);
+
+  useEffect(() => {
+    // Access elementRef.current to get the DOM element
+    const handleResize = () => {
+      const element = avatarRef.current;
+      if (element) {
+        // Perform operations on the element, such as getting its dimensions
+        const height = element.offsetHeight;
+        setAvatarHeight(height);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.addEventListener('resize', handleResize);
+    };
+  }, []);
+
+  console.log(avatarHeight);
+
   return (
     <article className="profilecard">
-      <div className="profilecard--border">
-        <img
-          src="https://brandweb.agency/wp-content/uploads/2023/04/brandweb_alexander.png"
-          alt="profile avatar"
-          className="profilecard__image"
-        />
+      <div
+        className="profilecard__avatar"
+        ref={avatarRef}
+        style={{ marginBottom: -avatarHeight / 2 + 8 }}
+      >
+        <div className="aspect-ratio">
+          <div>
+            <img
+              src="https://brandweb.agency/wp-content/uploads/2023/04/brandweb_alexander.png"
+              alt="profile avatar"
+            />
+          </div>
+        </div>
       </div>
       <div className="profilecard__content">
         <img src={profilemenu} alt="profile menu" className="profilecard__menu" />
